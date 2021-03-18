96fac724b41285656bc46775cb2adc47c3d4a98c
Your Name <you@example.com>
2021-03-18 10:47:27 +0300

    fixing loadbalance info

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
       "user" : "paul",
       // Multi host is possible, just by passing IPs/hostname as an array
       "host" : ["139.180.136.130"],
-      "ref"  : "origin/main",
+      "ref"  : "origin/test",
       "repo" : "git@github.com:SwiftSSH/v2rayserver.git",
       "path" : "/home/paul/v2rayserver",
       "post-deploy" : "pip3 install -r requirements.txt && npm install && npm run pm2-restart",
